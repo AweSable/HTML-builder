44cb844b930258e67134ab2c584ef91a04e37028
AweSable <DA.Poryvaev@gmail.com>
2021-11-08 19:22:46 +0300

    feat: completed the task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+//const process = require('process');
+const input = process.stdin;
+const output = process.stdout;
+const fullName = path.join(__dirname, 'text.txt');
+const ws = fs.createWriteStream(fullName, 'utf-8');
+const rl = readline.createInterface({input, output});
+
+console.log('Enter the text you want to write to the file:');
+rl.on('line', (input) => {
+	if (input == 'exit'){
+		rl.close();
+		ws.close();
+		console.log('Have a nice day');
+	} else ws.write(input + '\r\n');
+})
+rl.on('close', () => {
+	rl.close();
+	ws.close();
+	console.log('Have a nice day');
+})
