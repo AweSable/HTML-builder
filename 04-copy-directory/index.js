cef3a3d0c255f5ddbecf90793d671b059158bc59
AweSable <DA.Poryvaev@gmail.com>
2021-11-08 22:55:03 +0300

    feat: completed the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
+const folderNameOrigin = path.join(__dirname, 'files');
+const folderNameNew = path.join(__dirname, 'files-copy');
 
+fsPromises.mkdir(folderNameNew, {recursive: true});
+fs.readdir(folderNameOrigin, {encoding: 'utf8', withFileTypes: true}, (err, files) => {
+  if (err) console.log(err);
+  files.map(item => {
+    fs.copyFile(path.join(folderNameOrigin, item.name), path.join(folderNameNew, item.name), (err) => {
+      if (err) console.log(err);
+    });
+  });
+})
+
+fs.readdir(folderNameNew, {encoding: 'utf8', withFileTypes: true}, (err, files) => {
+  if (err) console.log(err);
+  files.map(item => {
+    fs.stat(path.join(folderNameOrigin, item.name), function(err, stat) {
+      if(err !== null) {
+        fs.unlink(path.join(folderNameNew, item.name), (err) => {
+          if (err) console.log(err);
+        });
+      }
+    });
+  });
+});
